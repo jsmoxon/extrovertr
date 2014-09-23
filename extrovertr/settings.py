fce23e82dbe6d4ce353ee64e1ac17fc2f8085ae6
Jack Moxon <jsmoxon@gmail.com>
2014-09-22 20:14:11 -0700

    static

--- extrovertr/settings.py
+++ extrovertr/settings.py
@@ -74,7 +74,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 #limits broker to free usage tier
 BROKER_POOL_LIMIT = 1
