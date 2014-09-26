a7468f197fa0332964e8452792562a14d58b0d58
Jack Moxon <jsmoxon@gmail.com>
2014-09-25 21:21:27 -0700

    security

--- pg/urls.py
+++ pg/urls.py
@@ -4,7 +4,7 @@ from pg import views
 
 urlpatterns = patterns('',
     url(r'^contacts/$', views.list_contacts, name='list_contacts'),
-    url(r'^contact/(?P<contact_id>\d+)/$', views.view_contact, name='view_contact'),
+    url(r'^contact/745(?P<contact_id>\d+)/$', views.view_contact, name='view_contact'),
     url(r'^contacts/add/$', views.add_contacts, name='add_contacts'),
-    url(r'^edit/(?P<contact_id>\d+)/$', views.edit_contact, name='edit_contact'),
+    url(r'^edit/745(?P<contact_id>\d+)/$', views.edit_contact, name='edit_contact'),
  )
